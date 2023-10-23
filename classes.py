93d95dc10c75ec9a7291fbea27a925ff57f164d7
caxhimel <mickbartel@gmail.com>
2023-10-23 12:47:54 +1000

    quiz now asks all questions and puts answers in right places

--- classes.py
+++ classes.py
@@ -2,7 +2,7 @@ import csv
 
 class User:
     def __init__(self):
-        self.user_scores = {"fe": 0, "fi": 0, "te": 0, "ti": 0, "ne": 0, "ni": 0, "se": 0, "si": 0}
+        self.user_scores = {"Fe": 0, "Fi": 0, "Te": 0, "Ti": 0, "Ne": 0, "Ni": 0, "Se": 0, "Si": 0}
         self.q_list = []
         self.iterator = 0
 
@@ -19,12 +19,14 @@ class User:
         self.user_name = input("Please enter your name: ")
 
     def _run_quiz(self):
-        print("Please enter a number from 1 - 5 to indicate how much you agree with each statement")
-        # Print the question
-        print(self.q_list[self.iterator])
-        self.answer = input()
-        # When user answers a question, store answer in the correct category
-        self.user_scores[self.q_list[self.iterator + 1]] = self.answer
+        while(self.iterator < len(self.q_list) - 1):
+            print("Please enter a number from 1 - 5 to indicate how much you agree with each statement")
+            # Print the question
+            print(self.q_list[self.iterator])
+            self.answer = input()
+            # When user answers a question, store answer in the correct category
+            self.user_scores[self.q_list[self.iterator + 1]] = self.answer
+            # Add 2 because we need to skip the answer sorting info for the question
+            self.iterator += 2
 
-        
 
